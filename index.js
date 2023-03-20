a0746d4975cea59743d43e22fc5ca2d52bf27d6b
Asier Goñi <asier@guten.tech>
2023-03-20 11:42:05 +0100

    Recuperamos los URL params y filtros

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // El siguiente archivo contiene el script que lanzamos a rurive.com en su buscador para filtrar y mostrar los resultados de la búsqueda.
 
-alert("Hola Mundo");
+// PASO 1 - Revisamos la URL, si hay existe el parámetro filtros, entonces lo guardamos en la variable filtros.
+const urlParams = new URLSearchParams(window.location.search);
+const filtros = urlParams.get('filtros');
+
+console.log(filtros);
